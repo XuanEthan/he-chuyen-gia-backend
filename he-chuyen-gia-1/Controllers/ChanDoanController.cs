279da2e4bfa9d4976ed0da90e954e530c5cd5600
XuanEthan <phgxuanhuy@gmail.com>
2025-09-11 10:54:02 +0700

    sua-tinh-nang(api): them code ham getDisease

--- he-chuyen-gia-1/Controllers/ChanDoanController.cs
+++ he-chuyen-gia-1/Controllers/ChanDoanController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using SbsSW.SwiPlCs;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,16 @@ namespace he_chuyen_gia_1.Controllers
             {
                 var res = _prolog.GetSymptoms();
                 return Ok(res);
+            }
+        }
+        [HttpPost("chuan-doan-benh")]
+        public IActionResult GetDisease([FromBody] string[] symptoms) 
+        { 
+            //input la 1 mang trieu chung
+            lock (_lock)
+            {
+                var result = _prolog.GetDiseases(symptoms);
+                return Ok(result);
             }
         }
     }
