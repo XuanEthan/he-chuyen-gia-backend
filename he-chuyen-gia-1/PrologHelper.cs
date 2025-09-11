a3b7128e7dadef971929558549c221453d88bbe6
XuanEthan <phgxuanhuy@gmail.com>
2025-09-11 10:58:14 +0700

    sua-tinh-nang: them code ham GetDisease

--- he-chuyen-gia-1/PrologHelper.cs
+++ he-chuyen-gia-1/PrologHelper.cs
@@ -1,4 +1,5 @@
 ﻿using SbsSW.SwiPlCs;
+using he_chuyen_gia_1.Entity;
 
 public class PrologHelper
 {
@@ -17,9 +18,52 @@ public class PrologHelper
         using (var q = new PlQuery("symptom(X)"))
         {
             foreach (var sol in q.SolutionVariables)
-                results.Add(sol["X"].ToString());
+            results.Add(sol["X"].ToString());
         }
         PlEngine.PlCleanup();
         return results;
     }
+
+    public List<Disease> GetDiseases(string[] symptoms)
+    {
+        var results = new List<Disease>();
+        var symptomList = string.Join(", ", symptoms);
+
+        // Query gọi sang Prolog
+        var query = $"diagnose_from_list([{symptomList}], Disease, Score, Matched, Unmatched)";
+
+        using (var q = new PlQuery(query))
+        {
+            foreach (PlQueryVariables sol in q.SolutionVariables)
+            {
+                var disease = new Disease
+                {
+                    Name = sol["Disease"].ToString(),
+                    Score =double.Parse(sol["Score"].ToString()),
+
+                    // Matched là list
+                    MatchedSymptoms = sol["Matched"]
+                        .ToString()
+                        .Trim('[', ']')
+                        .Split(',')
+                        .Select(s => s.Trim())
+                        .Where(s => !string.IsNullOrEmpty(s))
+                        .ToList(),
+
+                    // Unmatched là list
+                    UnmatchedSymptoms = sol["Unmatched"]
+                        .ToString()
+                        .Trim('[', ']')
+                        .Split(',')
+                        .Select(s => s.Trim())
+                        .Where(s => !string.IsNullOrEmpty(s))
+                        .ToList()
+                };
+                results.Add(disease);
+            }
+        }
+        PlEngine.PlCleanup();
+        return results;
+    }
+
 }
