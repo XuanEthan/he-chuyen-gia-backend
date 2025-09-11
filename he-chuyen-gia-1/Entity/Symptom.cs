bd3135939082854be674ec75f2ad477a7fc25121
XuanEthan <phgxuanhuy@gmail.com>
2025-09-11 10:56:19 +0700

    them-folder: them folder entity bao gom benh , trieu chung

---
+++ he-chuyen-gia-1/Entity/Symptom.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace he_chuyen_gia_1.Entity
+{
+    public class Symptom
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
